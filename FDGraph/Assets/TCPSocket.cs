e7a7a75438310923b83ff31af443f9af0b367577
Vasudha <vviswamu@asu.edu>
2017-10-16 14:43:34 -0700

    Decoding Stream into Objects

--- FDGraph/Assets/TCPSocket.cs
+++ FDGraph/Assets/TCPSocket.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if WINDOWS_UDP
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,19 +8,42 @@ using System.Diagnostics;
 using Windows.Networking.Sockets;
 using Windows.Storage.Streams;
 using Windows.Networking;
+using UnityEngine;
 
-namespace FDGraph.Assets
+namespace Assets
 {
+    public class Person
+    {
+        public string name;
+        public string group;
+        public float x;
+        public float y;
+        public float z;
+
+       
+    }
+
+    public class Link
+    {
+        public int from;
+        public int to;
+        public float weight;
+    }
+
+    
     public class TCPSocket
     {
         private StreamSocketListener listener;
         //HostName host = new HostName("192.168.253.100");
         int port = 7475;
+        public List<Person> persons = new List<Person>();
+        public List<Link> links = new List<Link>();
        
         public void Run()
         {
             try
             {
+                UnityEngine.Debug.Log("In here!!");
                 listener = new StreamSocketListener();
                 listener.Control.QualityOfService = SocketQualityOfService.LowLatency;
                 listener.ConnectionReceived += onConnection;
@@ -28,7 +52,7 @@ namespace FDGraph.Assets
             }
             catch(Exception e)
             {
-                Debug.WriteLine("There's an error! " + e);
+                System.Diagnostics.Debug.WriteLine("There's an error! " + e);
             }
             
         }
@@ -38,19 +62,20 @@ namespace FDGraph.Assets
             try
             {
                 await listener.BindServiceNameAsync(port.ToString());
-                //await listener.BindEndpointAsync(host,port.ToString());
-                Debug.WriteLine("Listening on port " + port);
+                //await listener.BindEndpointAsync(host,port.ToString
+                UnityEngine.Debug.Log("Listening on port" + port);
+                System.Diagnostics.Debug.WriteLine("Listening on port " + port);
             }
             catch (Exception e)
             {
                 listener.Dispose();
-                Debug.WriteLine(e.ToString());
+                System.Diagnostics.Debug.WriteLine(e.ToString());
             }
         }
 
         private async void onConnection(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
         {
-            Debug.WriteLine("Starting data read");
+            System.Diagnostics.Debug.WriteLine("Starting data read");
             DataReader reader = new DataReader(args.Socket.InputStream);
             try
             {
@@ -58,7 +83,7 @@ namespace FDGraph.Assets
             }
             catch(Exception e)
             {
-                Debug.WriteLine("Ending Connection!");
+                System.Diagnostics.Debug.WriteLine("Ending Connection!");
                 listener.Dispose();
             }
             
@@ -73,27 +98,46 @@ namespace FDGraph.Assets
             //await saveTime();
             while (true)
             {
-                DataReaderLoadOperation loadOp1 = reader.LoadAsync(1);
-                await loadOp1;
-                DataReaderLoadOperation loadOp = reader.LoadAsync(8);
+                DataReaderLoadOperation loadOp = reader.LoadAsync(1);
+                await loadOp;
+                if (loadOp.Status == Windows.Foundation.AsyncStatus.Completed)
+                {
+                    string type = getType(reader);
+                    System.Diagnostics.Debug.WriteLine("Type     " + type);
+                    loadOp = reader.LoadAsync(8);
+                    await loadOp;
+                    if (loadOp.Status == Windows.Foundation.AsyncStatus.Completed)
+                    {
+                        await getLength(reader, type);
+                        return;
+                    }
+                    //return;
+                }
+                /*DataReaderLoadOperation loadOp = reader.LoadAsync(8);
                 await loadOp;
                 if (loadOp.Status == Windows.Foundation.AsyncStatus.Completed)
                 {
                     await getLength(reader);
                     return;
-                }
+                }*/
             }
         }
 
-        private async Task getLength(DataReader reader)
+        string getType(DataReader reader)
+        {
+            string type = reader.ReadString(1);
+            return type;
+        }
+
+        private async Task getLength(DataReader reader, String type)
         {
             string msgLength = reader.ReadString(8);
             int length = Int32.Parse(msgLength);
-            Debug.WriteLine(length);
-            await getData(length, reader);
+            System.Diagnostics.Debug.WriteLine("Length   "+length);
+            await getData(length, reader, type);
         }
 
-        private async Task getData(int length, DataReader reader)
+        private async Task getData(int length, DataReader reader, String type)
         {
             //await saveTime();
             while (true)
@@ -103,18 +147,90 @@ namespace FDGraph.Assets
                 if (loadOp.Status == Windows.Foundation.AsyncStatus.Completed)
                 {
                     string data = reader.ReadString((uint)length);
-                    Debug.WriteLine(data);
+                    System.Diagnostics.Debug.WriteLine(data);
+                    if(type == "n")
+                    {
+                        String[] names = data.Split(';');
+                        
+                        for(int i = 0; i < names.Length - 1; i+=2)
+                        {
+                            Person p = new Person();
+                            p.name = names[i];
+                            p.group = names[i + 1];
+                            p.x = 0;
+                            p.y = 0;
+                            p.z = 0;
+                            persons.Add(p);
+                        }
+                        //Debug
+                        /*
+                        foreach (Person p in persons)
+                        {
+                            Debug.WriteLine(p.name);
+                            Debug.WriteLine(p.group);
+                            Debug.WriteLine(p.x);
+                            Debug.WriteLine(p.y);
+                            Debug.WriteLine(p.z);
+                        }
+                        */
+                    }
+                    if(type == "l")
+                    {
+                        String[] linkStr = data.Split(';');
+       
+                        for (int i = 0; i < linkStr.Length - 2; i += 3)
+                        {
+                            Link l = new Link();
+                            l.from = Int32.Parse(linkStr[i]);
+                            l.to = Int32.Parse(linkStr[i + 1]);
+                            l.weight = float.Parse(linkStr[i + 2]);
+                            links.Add(l);  
+                        }
+                        //Debug
+                        /*
+                        foreach(Link l in links)
+                        {
+                            Debug.WriteLine(l.from);
+                            Debug.WriteLine(l.to);
+                            Debug.WriteLine(l.weight);
+                        }
+                        */         
+                    }
+                    if(type == "p")
+                    {
+                        
+                        String[] posStr = data.Split(';');
+
+                        for (int i = 0; i< persons.Count; i++)
+                        {
+                            persons[i].x = float.Parse(posStr[i]);
+                            persons[i].y = float.Parse(posStr[i + 1]);
+                            persons[i].z = float.Parse(posStr[i + 2]);
+                        }
+                        //Debug
+                        
+                        foreach(Person p in persons)
+                        {
+                            System.Diagnostics.Debug.WriteLine(p.name);
+                            System.Diagnostics.Debug.WriteLine(p.group);
+                            System.Diagnostics.Debug.WriteLine(p.x);
+                            System.Diagnostics.Debug.WriteLine(p.y);
+                            System.Diagnostics.Debug.WriteLine(p.z);
+                        }
+                        
+                        
+                    }
                     //return;
                 }
-                /*try
+                try
                 {
                     await listen(reader);
                 }
                 catch(Exception e)
                 {
-                    Debug.WriteLine("Came here!");
+                    System.Diagnostics.Debug.WriteLine("Came here!");
                     listener.Dispose();
-                }*/
+                }
                 
             }
             
@@ -123,3 +239,4 @@ namespace FDGraph.Assets
 
     }
 }
+#endif
